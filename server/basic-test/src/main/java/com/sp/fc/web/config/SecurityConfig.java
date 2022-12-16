a51aa08d330fb101ec6192f6754d0bd8ee33387e
Sudal <jmjang88@naver.com>
2022-12-16 18:16:14 +0900

    basic login

--- server/basic-test/src/main/java/com/sp/fc/web/config/SecurityConfig.java
+++ server/basic-test/src/main/java/com/sp/fc/web/config/SecurityConfig.java
@@ -1,50 +1,76 @@
 package com.sp.fc.web.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
+import java.util.ArrayList;
+import java.util.List;
 
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@Configuration
+@EnableWebSecurity(debug = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true) // PrePost로 권한 체크 시작
+public class SecurityConfig{
 
+    // void configure(AuthenticationManagerBuilder auth) --> InMemoryUserDetailsManager()
     @Bean
-    UserDetailsService userService(){
-        final PasswordEncoder pw = passwordEncoder();
-        UserDetails user2 = User.builder()
-                .username("user2")
-                .password(pw.encode("1234"))
-                .roles("USER").build();
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password(pw.encode("1234"))
-                .roles("ADMIN").build();
-        return new InMemoryUserDetailsManager(user2, admin);
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        List<UserDetails> userDetailsList = new ArrayList<>();
+
+        userDetailsList.add(User.builder()
+                        .username("user2")
+                        .password(passwordEncoder().encode("2222"))
+                        .roles("USER")
+                        .build());
+
+        userDetailsList.add(User.builder()
+                        .username("admin")
+                        .password(passwordEncoder().encode("0000"))
+                        .roles("ADMIN")
+                        .build());
+
+        return new InMemoryUserDetailsManager(userDetailsList);
+    }
+
+    // WebSecurityConfigurerAdapter/ HttpSecurity --> SecurityFilterChain(Bean 등록)
+    // Chain으로 연결된 filter들 사이사이에 만든 filter를 껴넣는 method
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated();
+
+        http
+                .formLogin()
+                .loginPage("/login.html")
+                .loginProcessingUrl("/login")
+                .defaultSuccessUrl("/user-page")
+                .failureUrl("/login-error");
+
+        http
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/")
+                .invalidateHttpSession(true);
+
+        http
+                .httpBasic();
+
+        return http.build();
     }
 
     @Bean
-    PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .formLogin()
-                .and()
-                .authorizeRequests(auth->{
-                    auth.anyRequest().permitAll()
-                            ;
-                })
-                ;
-    }
 }
--- server/login-basic/src/main/java/com/sp/fc/web/config/SecurityConfig.java
+++ server/login-basic/src/main/java/com/sp/fc/web/config/SecurityConfig.java
@@ -1,21 +1,112 @@
 package com.sp.fc.web.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CsrfFilter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @EnableWebSecurity(debug = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
 
+    CsrfFilter csrfFilter;
+    UsernamePasswordAuthenticationFilter filter;
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Autowired
+    private CustomAuthDetails customAuthDetails;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http
-                .authorizeRequests(request->{
-                    request
-                            .antMatchers("/**").permitAll()
-                            ;
-                })
-                ;
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated();
+
+        http
+                .formLogin()
+                .loginPage("/login").permitAll() // 무한루프 방지(로그인 -> 에러 -> 로그인 -> 에러)
+                .defaultSuccessUrl("/")
+                .failureUrl("/login-error")
+                .authenticationDetailsSource(customAuthDetails);
+
+        http
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/")
+                .invalidateHttpSession(true);
+
+        // 접근권한 외의 페이지를 방문하려 할 때
+        http
+                .exceptionHandling()
+                .accessDeniedPage("/access-denied");
+
+
+        return http.build();
     }
+
+    /**
+     * void configure(WebSecurity web) 대체
+     * @return
+     */
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .requestMatchers(
+                        PathRequest.toStaticResources().atCommonLocations()
+                );
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        List<UserDetails> userDetailsList = new ArrayList<>();
+
+        userDetailsList.add(User.builder()
+                .username("user2")
+                .password(passwordEncoder().encode("2222"))
+                .roles("USER")
+                .build());
+
+        userDetailsList.add((User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("0000")))
+                .roles("ADMIN")
+                .build());
+
+        return new InMemoryUserDetailsManager(userDetailsList);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    /**
+     * Admin은 User의 모든 것 가능
+     * @return
+     */
+    @Bean
+    RoleHierarchy roleHierarchy(){
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+        roleHierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER");
+        return roleHierarchy;
+    }
+
 }
